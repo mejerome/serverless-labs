7c2561426a66286692187b26a73409d0a6068379
Jerome Tabiri <jtabiri@gmail.com>
2020-12-28 11:41:16 +0000

    completed basic-auth

--- basic-auth/src/Profile.js
+++ basic-auth/src/Profile.js
@@ -1,29 +1,47 @@
 import React, { useState, useEffect } from 'react'
-import { Auth } from 'aws-amplify'
-import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react'
+import { Button } from 'antd'
+import { Auth, Hub } from 'aws-amplify'
+// import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react'
 import Container from './Container'
+import Form from './Form'
 
 function Profile() {
   useEffect(() => {
     checkUser()
+    Hub.listen('auth', (data) => {
+      const { payload } = data
+      if (payload.event === 'signOut') {
+        setUser(null)
+      }
+    })
   }, [])
-  const [user, setUser] = useState({})
+  const [user, setUser] = useState(null)
   async function checkUser() {
     try {
       const data = await Auth.currentUserPoolUser()
       const userInfo = { username: data.username, ...data.attributes, }
       setUser(userInfo)
-    } catch (err) { console.log('error: ', err) }
+    } catch (err) {
+      console.log('error: ', err)
+    }
   }
-  return (
-    <Container>
-      <h1>Profile</h1>
-      <h2>Username: {user.username}</h2>
-      <h3>Email: {user.email}</h3>
-      <h4>Phone: {user.phone_number}</h4>
-      <AmplifySignOut />
-    </Container>
-  );
+  function signOut() {
+    Auth.signOut()
+    .catch(err =>
+      console.log('error signing out:', err ))
+    }
+  if (user) {
+    return (
+      <Container>
+        <h1>Profile</h1>
+        <h2>Username: {user.username}</h2>
+        <h3>Email: {user.email}</h3>
+        <h4>Phone: {user.phone_number}</h4>
+        <Button onClick={signOut}>Sign Out</Button>
+      </Container>
+    );
+  }
+  return <Form setUser={setUser} />
 }
 
-export default withAuthenticator(Profile)
+export default Profile
