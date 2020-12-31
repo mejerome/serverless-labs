7defdb68da92cb69d97f8e34ec52e72540261c2b
Jerome Tabiri <jtabiri@gmail.com>
2020-12-31 14:44:05 +0000

    festival app to be contined

---
+++ festivalapp/amplify/backend/function/festivalapp87cb07b0PostConfirmation/src/add-to-group.js
@@ -0,0 +1,38 @@
+/* eslint-disable-line */ const aws = require('aws-sdk');
+
+exports.handler = async (event, context, callback) => {
+  const cognitoProvider = new aws.CognitoIdentityServiceProvider({ apiVersion: '2016-04-18' });
+  let isAdmin = false
+  const adminEmails = ['mejerome19@gmail.com', 'jtabiri@gmail.com']
+  if (adminEmails.indexOf(event.request.userAttributes.email) !== -1) {
+    isAdmin = true
+    }
+
+  const groupParams = {
+    UserPoolId: event.userPoolId,
+  };
+
+  const addUserParams = {
+    UserPoolId: event.userPoolId,
+    Username: event.userName,
+  };
+  if (isAdmin) {
+    groupParams.GroupName = 'Admin',
+    userParams.GroupName = 'Admin'
+
+  try {
+    await cognitoProvider.getGroup(groupParams).promise();
+  } catch (e) {
+    await cognitoProvider.createGroup(groupParams).promise();
+  }
+
+  try {
+    await cognitoProvider.adminAddUserToGroup(addUserParams).promise();
+    callback(null, event);
+  } catch (e) {
+    callback(e);
+  }
+} else {
+  callback(null, event)
+}
+}
